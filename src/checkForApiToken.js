c72efc17b2d2a80ba69801be09b9f80cc045734c
Marcos-Dantas <kisnaiver1223@gmail.com>
2024-06-09 20:19:15 -0300

    configuração

--- src/checkForApiToken.js
+++ src/checkForApiToken.js
@@ -1,11 +1,9 @@
 module.exports = (req, res, next) => {
     const apiToken = req.header("x-api-key");
-    console.log(apiToken)
-    console.log(process.env.API_TOKEN)
-    console.log(String(process.env.API_TOKEN) == String(apiToken))
-    if (String(process.env.API_TOKEN) != String(apiToken)) {
+    if (String(process.env.API_TOKEN) == String(apiToken)) {
+        next();
+    }else {
         next(new Error("Unauthorized."));
         return;
     }
-    next();
 }
