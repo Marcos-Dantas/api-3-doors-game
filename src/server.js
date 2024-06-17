48bde0731c9bd4e5f4cd0592cc0d40edcff9811a
Marcos-Dantas <kisnaiver1223@gmail.com>
2024-06-17 17:07:12 -0300

    criado os modelos User e Player para armazenar as informações do usuario e do jogador

--- src/server.js
+++ src/server.js
@@ -1,7 +1,33 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv"
+import {body, validationResult} from "express-validator";
 
+export const createUserValidator = [
+  body('email', 'Email não pode ser vazio').not().isEmpty(),
+  body('name', 'Nome não pode ser vazio').not().isEmpty(),
+  body('city', 'Cidade não pode ser vazio').not().isEmpty(),
+  body('state', 'Estado não pode ser vazio').not().isEmpty(),
+  body('age', 'Idade não pode ser vazio').not().isEmpty(),
+  body('email', 'Email invalido').isEmail(),
+  body('email').custom(async (email) => {
+    const user = await prisma.user.findUnique({
+      where: {email: email}
+    })
+    if (user) {
+      throw new Error('Email ja esta em uso')
+    } 
+  }),
+  body('password', 'password não pode ser vazio').not().isEmpty(),
+  body('password', 'O comprimento mínimo da senha é de 6 caracteres').isLength({min: 6}),
+]
+
+export const loginValidator = [
+  body('email', 'Email não pode ser vazio').not().isEmpty(),
+  body('email', 'Email invalido').isEmail(),
+  body('password', 'password não pode ser vazio').not().isEmpty(),
+  body('password', 'O comprimento mínimo da senha é de 6 caracteres').isLength({min: 6}),
+]
 const prisma = new PrismaClient();
 
 var app = express();
@@ -9,54 +35,50 @@ app.use(express.json());
 
 dotenv.config();
 
-// Rota para obter todos os usuários
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany({});
-  res.status(200).json(users)
-});
-  
 // Rota para criar um novo usuário
-app.post('/users', async (req, res) => {
-const { name, email} = req.body;
-const newUser = await prisma.user.create({
-  data: { name, email},
-});
-res.status(201).json(newUser)
+app.post('/users', createUserValidator, async (req, res) => {
+  const errors = validationResult(req)
+  if (errors.isEmpty()) {
+    const { name, email, password, city, state, age} = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+        name: name, 
+        password: password, 
+        city: city, 
+        state: state, 
+        age: parseInt(age),
+      },
+    });
+    res.status(201).json(newUser)
+  }else {
+    res.status(422).json({errors: errors.array()})
+  }
 });
 
-// Rota para pegar um usuário especifico
-app.get('/user/:id', async (req, res) => {
-  try {
+// Rota para login
+app.post('/user/', loginValidator, async (req, res) => {
+  const errors = validationResult(req)
+  if (errors.isEmpty()) {
+    const {email, password} = req.body;
     const user = await prisma.user.findUnique({
-      where: {id: parseInt(req.params.id, 10)}
+      where: {email: email, password: password}
     })
     if (user) {
+      const newPlayer = await prisma.player.create({
+        data: {
+          userEmail: user.email, 
+        },
+      });
       res.json(user);
     } else {
-      res.status(404).json({ error: 'User not found' });
+      res.status(404).json({ error: 'Usuario não encontrado' });
     }
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+  }else {
+    res.status(422).json({errors: errors.array()})
   }
 });
 
-// Rota para deletar um usuário
-app.delete('/user/:id', async (req, res) => {
-  try {
-    const user = await prisma.user.delete({
-      where: {id: parseInt(req.params.id, 10)}
-    })
-    res.json({ message: 'Usuario deletado com sucesso', user });
-
-  } catch (error) {
-    if (error.code === 'P2025') { 
-      res.status(404).json({ error: 'User not found' });
-    } else {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  }
-});
-  
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
