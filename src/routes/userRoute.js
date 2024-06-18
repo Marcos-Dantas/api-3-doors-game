70566ecf20df5a9928a931fa2f7c3a9107df3d56
Marcos-Dantas <kisnaiver1223@gmail.com>
2024-06-17 23:20:02 -0300

    consertado problema no vercel, adicionado hash na senha do usuario para maior segurança

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,26 +1,33 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import userController from '../controllers/userController.js';
-import {loginValidator, createUserValidator} from '../validators/validators.js';
+import getValidators from '../validators/validators.js';
 import dotenv from "dotenv"
 import {validationResult} from "express-validator";
+import bcrypt from 'bcrypt';
 
 const routes = express.Router();
 
 const prisma = new PrismaClient();
+const { createUserValidator, loginValidator } = getValidators(prisma);
 
 dotenv.config();
 
 // Rota para criar um novo usuário
 routes.post('/users', createUserValidator, async (req, res) => {
   const errors = validationResult(req)
+
   if (errors.isEmpty()) {
     const { name, email, password, city, state, age} = req.body;
+    // Gerar o hash da senha
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    
     const newUser = await prisma.user.create({
       data: {
         email: email,
         name: name, 
-        password: password, 
+        password: hashedPassword, 
         city: city, 
         state: state, 
         age: parseInt(age),
@@ -31,9 +38,9 @@ routes.post('/users', createUserValidator, async (req, res) => {
         userEmail: newUser.email, 
       },
     });
-    res.status(201).json(newUser)
+    return res.status(201).json(newUser)
   }else {
-    res.status(422).json({errors: errors.array()})
+    return res.status(422).json({errors: errors.array()})
   }
 });
 
@@ -42,16 +49,24 @@ routes.post('/user/', loginValidator, async (req, res) => {
   const errors = validationResult(req)
   if (errors.isEmpty()) {
     const {email, password} = req.body;
+    
     const user = await prisma.user.findUnique({
-      where: {email: email, password: password}
+      where: {email: email}
     })
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ error: 'Usuario não encontrado' });
+    
+    if (!user) {
+      return res.status(404).json({ error: 'Usuario não encontrado' });
+    } 
+    
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(404).json({ error: 'Senha inválida' });
     }
+
+    return res.json(user);
+
   }else {
-    res.status(422).json({errors: errors.array()})
+    return res.status(422).json({errors: errors.array()})
   }
 });
 
--- src/validators/validators.js
+++ src/validators/validators.js
@@ -1,29 +1,30 @@
 import {body} from "express-validator";
 
-export const createUserValidator = [
-    body('email', 'Email não pode ser vazio').not().isEmpty(),
-    body('name', 'Nome não pode ser vazio').not().isEmpty(),
-    body('city', 'Cidade não pode ser vazio').not().isEmpty(),
-    body('state', 'Estado não pode ser vazio').not().isEmpty(),
-    body('age', 'Idade não pode ser vazio').not().isEmpty(),
-    body('email', 'Email invalido').isEmail(),
-    body('email').custom(async (email) => {
-        const user = await prisma.user.findUnique({
-        where: {email: email}
-        })
-        if (user) {
-        throw new Error('Email ja esta em uso')
-        } 
-    }),
-    body('password', 'password não pode ser vazio').not().isEmpty(),
-    body('password', 'O comprimento mínimo da senha é de 6 caracteres').isLength({min: 6}),
-]
-  
-export const loginValidator = [
-    body('email', 'Email não pode ser vazio').not().isEmpty(),
-    body('email', 'Email invalido').isEmail(),
-    body('password', 'password não pode ser vazio').not().isEmpty(),
-    body('password', 'O comprimento mínimo da senha é de 6 caracteres').isLength({min: 6}),
-]
+export const getValidators = (prisma) => ({ 
+    createUserValidator:  [
+        body('email', 'Email não pode ser vazio').not().isEmpty(),
+        body('name', 'Nome não pode ser vazio').not().isEmpty(),
+        body('city', 'Cidade não pode ser vazio').not().isEmpty(),
+        body('state', 'Estado não pode ser vazio').not().isEmpty(),
+        body('age', 'Idade não pode ser vazio').not().isEmpty(),
+        body('email', 'Email invalido').isEmail(),
+        body('email').custom(async (email) => {
+            const user = await prisma.user.findUnique({
+            where: {email: email}
+            })
+            if (user) {
+            throw new Error('Email ja esta em uso')
+            } 
+        }),
+        body('password', 'password não pode ser vazio').not().isEmpty(),
+        body('password', 'O comprimento mínimo da senha é de 6 caracteres').isLength({min: 6}),
+    ],
+    loginValidator: [
+        body('email', 'Email não pode ser vazio').not().isEmpty(),
+        body('email', 'Email invalido').isEmail(),
+        body('password', 'password não pode ser vazio').not().isEmpty(),
+        body('password', 'O comprimento mínimo da senha é de 6 caracteres').isLength({min: 6}),
+    ]
+});
 
-export default {loginValidator, createUserValidator};
+export default getValidators;
