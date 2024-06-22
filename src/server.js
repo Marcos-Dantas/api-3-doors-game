3d717a947245296ab2df266fbb09788cde97868c
Marcos-Dantas <kisnaiver1223@gmail.com>
2024-06-22 12:39:20 -0300

    adicionado novamente o css, para mostrar a pagina de docs

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerSpecs from '../swaggerConfig.js';
 
 // CDN CSS
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.3/swagger-ui.min.css";
 
 const PORT = 3000;
 var app = express();
 
 // Rota para documentação
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs, { customCssUrl: CSS_URL }));
 // app.use(
 //   "/api-docs",
 //   swaggerUI.serve,
